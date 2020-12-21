e885333d2048486885c492666b7170f3f85588a9
Tomas Hlavacek <tmshlvck@gmail.com>
2020-12-21 03:25:19 +0100

    Fix connectivity action
    Fix connectivity check and action
    Add example cfg file

--- watch.py
+++ watch.py
@@ -9,7 +9,6 @@ import sys
 import os
 
 debug = False
-#debug = True
 retrytime = 2
 
 def d(msg):
@@ -22,7 +21,7 @@ def lima(t):
 def ping(host):
     d("Ping host %s" % host)
     try:
-        subprocess.check_output(["ping", "-c", "1", host])
+        subprocess.check_output(["ping", "-c", "1", host], stderr=subprocess.STDOUT)
         d("... ok")
         return True                      
     except:
@@ -43,7 +42,8 @@ def notify(email, subj, body):
     d("Sending notify subj=%s body=%s to %s" % (subj, body, email))
 
     bbody = body.encode()
-    subprocess.run(["mail", "-s '%s'" % subj, email], input=bbody)
+    #subprocess.run(["mail", "-s '%s'" % subj, email], input=bbody)
+    subprocess.run(["echo", "-s '%s'" % subj, email], input=bbody)
 
 
 def exec(command):
@@ -51,32 +51,37 @@ def exec(command):
     os.system(command)
 
 
-def is_scheduled(hdata, delay, lastaction, t):
-    return ((int(hdata['lastchange'])+delay) < t and lastaction < int(hdata['lastchange']))
+def is_scheduled(lastchange, delay, lastaction, t):
+    return ((lastchange+delay) < t and lastaction < lastchange)
 
 def handle_down_notifies(hconf, hdata, t):
     ntf = hconf.get('notify', [])
-    delay = hconf.get('notify_delay', 0)
+    delay = int(hconf.get('notify_delay', 0))
     last = int(hdata.get('lastnotify', 0))
     
-    if is_scheduled(hdata, delay, last, t):
+    if is_scheduled(int(hdata['lastchange']), delay, last, t):
         for n in ntf:
-            notify(n, 'host % is down for %d seconds' % (hconf['hostname'], delay), str(dta[hn]))
+            notify(n, 'host %s is down for %d seconds' % (hconf['hostname'], delay), str(hdata))
         hdata['lastnotify'] = t
         hdata['lastnotify_lima'] = lima(t)
+    else:
+      d("... notify not scheduled")
 
 def handle_hostdown(hconf, hdata, t):
     exe = hconf.get('exec', None)
     delay = int(hconf.get('exec_delay', 0))
     last = int(hdata.get('lastexec', 0))
 
-    d("Consider exec=%s lastchange=%d, delay=%d lastexec=%d" % (str(exe), int(hdata['lastchange']), delay, last))
-    if exe and is_scheduled(hdata, delay, last, t):
+    d("Considering host exec=%s lastchange=%d, delay=%d lastexec=%d" % (str(exe), int(hdata['lastchange']), delay, last))
+    if exe:
+      if is_scheduled(int(hdata['lastchange']), delay, last, t):
         exec(exe)
         hdata['lastexec'] = t
         hdata['lastexec_lima'] = lima(t)
+      else:
+        d("... execution not scheduled")
     else:
-        d("... no execution.")
+        d("... nothing to execute")
 
     handle_down_notifies(hconf, hdata, t)
 
@@ -85,26 +90,52 @@ def handle_hostup(hconf, hdata, t):
     pass
 
 
+def handle_connectivitydown(cconf, cdata, t):
+    exe = cconf.get('exec', None)
+    delay = int(cconf.get('exec_delay', 0))
+    last = int(cdata.get('lastexec', 0))
+
+    d("Consider connectivity exec=%s lastchange=%d, delay=%d lastexec=%d" % (str(exe), int(cdata['lastchange']), delay, last))
+    if exe:
+      if is_scheduled(int(cdata['lastchange']), delay, last, t):
+        exec(exe)
+        hdata['lastexec'] = t
+        hdata['lastexec_lima'] = lima(t)
+      else:
+        d("... execution not scheduled")
+    else:
+        d("... nothing to execute")
+
+
 @click.command()
 @click.option('-d', '--debug', 'debugparam', is_flag=True)
-@click.argument('config', type=click.File('r'))
-@click.argument('datafile', type=click.Path())
+@click.argument('config', type=click.File('r'), default="cfg.yml")
+@click.argument('datafile', type=click.Path(), default="data.yml")
 def main(debugparam, config, datafile):
     """
-    config:::
+    config:
     ---
     hosts:
       - hostname: mamut.d.taaa.eu
       - hostname: mamut.d.taaa.eu
       - hostname: chapadlo
-        notify_down:
+        notify:
           - tmshlvck@gmail.com
       - hostname: lkko
-        notify_all:
+        notify:
           - tmshlvck@gmail.com
         notify_delay: 600
         exec: "nohup reboot"
         exec_delay: 3600
+    coonectivity:
+      ping:
+        - krtek.taaa.eu
+        - www.google.com
+        - www.seznam.cz
+        - taz.core.ignum.cz
+        - www.centrum.cz
+      exec_delay: 600
+      exec: "nohup reboot"
     """
     global debug
     if debugparam:
@@ -142,6 +173,23 @@ def main(debugparam, config, datafile):
             handle_hostdown(h, dta[hn], t)
         else:
             handle_hostup(h, dta[hn], t)
+
+    if 'connectivity' in cfg:
+      d('Checking local connectivity...')
+      if not 'local_connectivity' in dta:
+        dta['local_connectivity'] = {}
+
+      for h in cfg['connectivity']['ping']:
+        if testping(h):
+          dta['local_connectivity']['laststate'] = True
+          dta['local_connectivity']['lasthit'] = h
+          break
+      else:
+        dta['local_connectivity']['laststate'] = False
+        handle_connectivitydown(cfg['connectivity'], dta['local_connectivity'], t)
+
+      dta['local_connectivity']['lastcheck'] = t
+      dta['local_connectivity']['lastcheck_lima'] = lima(t)
 
     d("Save results...")
     with open(datafile, 'w') as dfh:
